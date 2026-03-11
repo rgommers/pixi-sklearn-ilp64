25fd181a1d840c8ab7a075d634d36784b8f6f6a5
Ralf Gommers <ralf.gommers@gmail.com>
2026-03-11 21:26:52 +0100

    Get scipy + scikit-learn to build (LP64); all tests pass
    Run `pixi r build` followed by `pixi r test -j4`

--- pixi-packages/scipy/build.sh
+++ pixi-packages/scipy/build.sh
@@ -3,7 +3,7 @@ mkdir builddir
 $PYTHON -m build -w -n -x \
     -Cbuild-dir=builddir \
     -Cinstall-args=--tags=runtime,python-runtime,devel \
-    -Csetup-args=-Dblas=mkl-dynamic-ilp64-gomp \
+    -Csetup-args=-Dblas=mkl-dynamic-lp64-seq \
     -Csetup-args=-Duse-g77-abi=true
 
 $PYTHON -m pip install dist/scipy*.whl
